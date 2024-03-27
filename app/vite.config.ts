dc475a062aaacde3c942b70b5e5ef9fbd4de49c0
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2024-03-27 16:05:17 +0100

    use NODE_ENV instead of Mode

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-const base = process.env.Mode === "production" ? "/roughlify/" : "/";
+const base = process.env.NODE_ENV === "production" ? "/roughlify/" : "/";
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("base:", base);
 
 // https://vitejs.dev/config/
 export default defineConfig({
