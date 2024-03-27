a2418e504948fefd6f5bf3106f1a21c58fa50d67
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2024-03-27 15:49:50 +0100

    fix vite config for gh deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,8 +2,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
+const base = process.env.Mode === "production" ? "/roughlify/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
   resolve: {
     alias: {
